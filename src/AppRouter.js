3ae16a8f4c039c682a56d600f8c5ea8ada2249fc
Surya <surya@emqube.com>
2024-04-24 15:18:02 +0530

    Routes Issue

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -8,36 +8,42 @@ import Posts from './components/Posts';
 // import NotFound from './components/NotFound';
 function NoMatch() {
     return (
-      <div style={{ padding: 20 }}>
-        <h2>404: Page Not Found</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-      </div>
+        <div style={{ padding: 20 }}>
+            <h2>404: Page Not Found</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+        </div>
     );
-  }
+}
 
-  const router = createBrowserRouter([
+const router = createBrowserRouter([
     {
-        path:"/",
-        element:<Home />,
-    },{
-        path:"posts",
-        element:<Posts />,
-    },{
-        path:"*",
-        element:<NoMatch />
+        path: "/",
+        element: <Home />,
+        children: [
+            {
+                path: "posts",
+                element: <Posts />,
+            }
+        ]
+    }, {
+        path: "posts",
+        element: <Posts />,
+    }, {
+        path: "*",
+        element: <NoMatch />
     }
-  ])
+])
 const AppRouter = () => {
-  return (
-    <RouterProvider router={router}/>
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //      <Route path="posts" element={<Posts />} />
-    //     <Route path="*" element={<NoMatch />} />
-    //   </Routes>
-    // </Router>
-  );
+    return (
+        <RouterProvider router={router} />
+        // <Router>
+        //   <Routes>
+        //     <Route path="/" element={<Home />} />
+        //      <Route path="posts" element={<Posts />} />
+        //     <Route path="*" element={<NoMatch />} />
+        //   </Routes>
+        // </Router>
+    );
 };
 
 export default AppRouter;
