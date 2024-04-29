07c080402da69597e92f25757e95d51fd140b515
Surya <surya@emqube.com>
2024-04-29 16:45:25 +0530

    Worked on routes issue

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,48 +1,38 @@
 // AppRouter.js
 import React from 'react';
-// import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './components/Home';
-import Posts from './components/Posts';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/layout'
+import Home from './pages/Home';
+import Posts from './pages/Posts';
 // import Posts from './components/Posts';
-// import NotFound from './components/NotFound';
-function NoMatch() {
-    return (
-        <div style={{ padding: 20 }}>
-            <h2>404: Page Not Found</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-        </div>
-    );
-}
+import NoPage from "./pages/NoMatch";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-        children: [
-            {
-                path: "posts",
-                element: <Posts />,
-            }
-        ]
-    }, {
-        path: "posts",
-        element: <Posts />,
-    }, {
-        path: "*",
-        element: <NoMatch />
-    }
-])
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <Home />,
+//     }, {
+//         path: "posts",
+//         element: <Posts />,
+//     }, {
+//         path: "*",
+//         element: <NoMatch />
+//     }
+// ])
 const AppRouter = () => {
     return (
-        <RouterProvider router={router} />
-        // <Router>
-        //   <Routes>
-        //     <Route path="/" element={<Home />} />
-        //      <Route path="posts" element={<Posts />} />
-        //     <Route path="*" element={<NoMatch />} />
-        //   </Routes>
-        // </Router>
+        // <RouterProvider router={router} />
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="posts" element={<Posts />} />
+                    {/* <Route path="contact" element={<Contact />} /> */}
+                    <Route path="*" element={<NoPage />} />
+                </Route>
+            </Routes>
+        </Router>
     );
 };
 
