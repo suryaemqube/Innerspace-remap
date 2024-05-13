f274e5b037d5b28c66fe9ec0f10671b19e858b91
Surya <surya@emqube.com>
2024-05-13 15:28:03 +0530

    Prefetch Data

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,18 @@
 import Header from "./Header";
 import Footer from "./Footer";
+
+import "swiper/css/bundle";
+import "swiper/css/navigation";
+
+import "../assets/css/normalize.css";
+import "../assets/css/home-common-responsive.css";
+import "../assets/css/main.css";
+import "../assets/css/inside.css";
+import "../assets/css/identity.css";
+import "../assets/css/brands.css";
+import "../assets/css/last-chance-archive.css";
+
+import "../assets/css/uifixer.css";
 const Layout = ({ children }) => {
   return (
     <>
